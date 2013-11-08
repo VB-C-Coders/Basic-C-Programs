47f1eb789c8f950fee2322fefcef9637d5d733a5
quet00 <quetzerozero@gmail.com>
2013-11-08 08:37:30 -0500

    Added some of dem commentz

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h" //Don't worry about these
 #include <iostream> //The io stands for input/output
 
-int main() //This is the main function.  This is where you start to write code
+int main() //This is the main function.  This is where you start to write code and where it will start the exe.
 {
   std::cout << "Hello World! \n"; //This prints hello to the screen
   system("Pause"); //This pauses the terminal window until a key is pressed
